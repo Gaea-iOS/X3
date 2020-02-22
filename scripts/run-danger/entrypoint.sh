ce9f18f8d8b7013d6234eca1811d00617243dc2c
jerrywang <445242970@qq.com>
2020-02-23 00:36:44 +0800

    update node first

--- scripts/run-danger/entrypoint.sh
+++ scripts/run-danger/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
 # Install danger cli on the machine
-yarn global add danger
+yarn add danger --dev
 
 # install needed dependencies
 yarn
